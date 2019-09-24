5fa945c39e0dd50e65b173a80a6c0abea7f7bd76
ryooku <pisx2.kayane.wvw@gmail.com>
2019-09-24 16:26:42 +0900

    refactoring

--- main.js
+++ main.js
@@ -1,44 +1,27 @@
-function getFeedURL() {
-  return 'https://www.google.com/alerts/feeds/05376717576481271219/10876191608748414710';
-}
-
-function getChatWorkToken() {
-  return 'e3b36529a7391fc252a209a3603ca111';
-}
-
-function testBot(message) {
-  var client = ChatWorkClient.factory({token:"e3b36529a7391fc252a209a3603ca111"});
+function sendChatworkMessage(message) {
+  var properties = PropertiesService.getScriptProperties();
+  var client = ChatWorkClient.factory({token: properties.getProperty('CW_TOKEN')});
   client.sendMessage({
-    room_id: 127063991,
+    room_id: properties.getProperty('CW_ROOM_ID'),
     body: "[info]" + message + "[/info]"
   });
 }
 
-function getGoogleAlert() {
-  var feedURL = getFeedURL();
+function sendSEONews() {
+  var properties = PropertiesService.getScriptProperties();
   var atom = XmlService.getNamespace('http://www.w3.org/2005/Atom');
-  var document = XmlService.parse(UrlFetchApp.fetch(feedURL).getContentText());
+  var document = XmlService.parse(UrlFetchApp.fetch(properties.getProperty('FEED_URI')).getContentText());
   var items = document.getRootElement().getChildren('entry', atom);
 
+  if (items.length < 1) return;
 
+  var message = "(*)本日のSEOニュース(*)\n\n";
   for(var i = 0; i < items.length; i++) {
-    //サイトのURLを取得
     var link = items[i].getChild('link', atom).getAttribute('href').getValue();
-    //サイトのタイトルを取得
     var title = items[i].getChild('title', atom).getText();
-    //URLとタイトルを連結
-    if (i == 0){
-      var message = "(*)今日のニュースをお届けします！\n" + title + "\n" + link;
-    }else{
-      var message = title + "\n" + link;
-    }
-    //HTMLタグを除去してチャットワークへ投稿
-    Logger.log(message);
-    testBot(message);
-
-
-    // client.sendMessage({room_id: room_id, body: message.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'')});
+    var content = items[i].getChild('content', atom).getText();
+    message +=  "------------------------------\n" + title + "\n" + content + "\n" + link + "\n";
   }
 
-
+  sendChatworkMessage(message);
 }
